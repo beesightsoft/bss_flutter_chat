c6b963f4e497f6b7e465cececb902e0be7a6d6b3
Duy Tran <tranquangduy7994@gmail.com>
2018-08-27 13:57:27 +0700

    first init

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_bss_chat
+
+A Flutter chat app for Beesightsoft staffs
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/login.dart
@@ -0,0 +1,164 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bss_chat/const.dart';
+import 'package:flutter_bss_chat/main.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BSS chat',
+      theme: new ThemeData(
+        primaryColor: themeColor,
+      ),
+      home: LoginScreen(title: 'BSS CHAT'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  LoginScreenState createState() => new LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences prefs;
+
+  bool isLoading = false;
+  FirebaseUser currentUser;
+
+  Future<Null> handleSignIn() async {
+    prefs = await SharedPreferences.getInstance();
+
+    if (prefs.getString('id') != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen(currentUserId: prefs.getString('id'))),
+      );
+    } else {
+      this.setState(() {
+        isLoading = true;
+      });
+
+      GoogleSignInAccount googleUser = await googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      if (firebaseUser != null) {
+        // Check is already sign up
+        final QuerySnapshot result =
+            await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
+        final List<DocumentSnapshot> documents = result.documents;
+        if (documents.length == 0) {
+          // Update data to server if user is new
+          Firestore.instance.collection('users').document(firebaseUser.uid).setData(
+              {'nickname': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl, 'id': firebaseUser.uid});
+
+          // Write data to local
+          currentUser = firebaseUser;
+          await prefs.setString('id', currentUser.uid);
+          await prefs.setString('nickname', currentUser.displayName);
+          await prefs.setString('photoUrl', currentUser.photoUrl);
+        } else {
+          // Write data to local
+          await prefs.setString('id', documents[0]['id']);
+          await prefs.setString('nickname', documents[0]['nickname']);
+          await prefs.setString('photoUrl', documents[0]['photoUrl']);
+          await prefs.setString('aboutMe', documents[0]['aboutMe']);
+        }
+        Fluttertoast.showToast(msg: "Sign in success");
+        this.setState(() {
+          isLoading = false;
+        });
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MainScreen(
+                    currentUserId: firebaseUser.uid,
+                  )),
+        );
+      } else {
+        Fluttertoast.showToast(msg: "Sign in fail");
+        this.setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(
+                      'images/logo_bss.png',
+                      width: 200.0,
+                      height: 200.0,
+                      fit: BoxFit.contain,
+                    ),
+                    margin: EdgeInsets.only(bottom: 50.0),
+                  ),
+                  FlatButton(
+                    onPressed: handleSignIn,
+                    child: Text(
+                      'SIGN IN WITH GOOGLE',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    color: themeColor,
+                    highlightColor: themeColor2,
+                    splashColor: Colors.transparent,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ),
+
+            // Loading
+            Positioned(
+              child: isLoading
+                  ? Container(
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                        ),
+                      ),
+                      color: Colors.white.withOpacity(0.8),
+                    )
+                  : Container(),
+            ),
+          ],
+        ));
+  }
+}
