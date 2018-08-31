2a929c21d066ff219b147bf50f264c87c5741de4
Duy Tran <tranquangduy7994@gmail.com>
2018-08-31 18:08:45 +0700

    update

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,7 @@ import 'package:flutter_bss_chat/main.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 void main() => runApp(new MyApp());
 
